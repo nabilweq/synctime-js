04c17012a82a261e4dda9a15a3ab584db6fce1f0
nabilweq <nabeeltkanr@gmail.com>
2023-12-02 13:21:14 +0530

    ts updated

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+const getFormatedDate = (date:any) => {
+
+  if(!date) {
+      const date = new Date();
+      const GSToffSet = 330//240; //IST is 5:30
+      const offset = GSToffSet*60*1000;
+      const GSTTime = new Date(date.getTime()+offset);
+
+      return new Date(GSTTime.toJSON().substring(0, 10));
+  } else {
+      date = new Date(date);
+      const GSToffSet = 330//240; //IST is 5:30
+      const offset = GSToffSet*60*1000;
+      const GSTTime = new Date(date.getTime()+offset);
+
+      return new Date(GSTTime.toJSON().substring(0, 10));
+  }
+};
+
+const changeDate = (val:any, date:any='') => {
+  if(!date) {
+      const date = new Date();
+      const GSToffSet = 330//240;
+      const offset = GSToffSet*60*1000;
+      const GSTTime = new Date(date.getTime()+offset);
+      GSTTime.setDate(date.getDate() + val);
+
+      return new Date(GSTTime.toJSON().substring(0, 10))
+  } else {
+      const GSToffSet = 330//240;
+      const offset = GSToffSet*60*1000;
+      const GSTTime = new Date(date.getTime()+offset);
+      GSTTime.setDate(date.getDate() + val);
+
+      return new Date(GSTTime.toJSON().substring(0, 10))
+  }
+};
+
+const getCurrentTime = () => {
+  const date = new Date();
+  const GSToffSet = 330//240; //IST is 5:30
+  const offset = GSToffSet*60*1000;
+  const GSTTime = new Date(date.getTime()+offset);
+
+  return GSTTime;
+}
+
+const getLastMonthDays = () => {
+
+  const currentDate = new Date();
+  const lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+  const lastDayOfLastMonth = new Date(lastMonthDate.getFullYear(), lastMonthDate.getMonth() + 1, 0);
+  const numberOfDaysInLastMonth = lastDayOfLastMonth.getDate();
+
+  return numberOfDaysInLastMonth;
+}
+
+module.exports = {
+  getFormatedDate,
+  changeDate,
+  getCurrentTime,
+  getLastMonthDays
+};
